7f7b8227f1dc1022e4b93fc0cb79e284989911f8
Pola <polaskibinska@gmail.com>
2020-11-05 20:45:01 +0100

    Updates

---
+++ precache-manifest.090ed4be25fed32a3677092c70eed650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27ef35343be0bf28730003a80d89e4ed",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5b8f64405913015fa3d7",
+    "url": "./static/css/main.170dbdf5.chunk.css"
+  },
+  {
+    "revision": "a34d3f883443f8c04856",
+    "url": "./static/js/2.1742a9b6.chunk.js"
+  },
+  {
+    "revision": "3c73c585782ac05880c0f89bcfdbba5a",
+    "url": "./static/js/2.1742a9b6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5b8f64405913015fa3d7",
+    "url": "./static/js/main.50b60ccf.chunk.js"
+  },
+  {
+    "revision": "f8f3c6b421bf62a8dcc1",
+    "url": "./static/js/runtime-main.d73f11d5.js"
+  }
+]);
